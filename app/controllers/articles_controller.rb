f36e2fb63e812d69e514f12bc567b078fe6579e4
Sam Lim <limsammy1@gmail.com>
2017-06-06 22:01:19 -0700

    added delete functionality for show.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,12 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to action: "index"
+  end
+
     private
 
     def article_params
