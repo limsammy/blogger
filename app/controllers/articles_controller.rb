5a5947812f7fd1723456fd45885b785bc09df09f
Sam Lim <limsammy1@gmail.com>
2017-06-06 22:13:41 -0700

    added edit func and flaash to index layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :update]
+
   def index
     @articles = Article.all
   end
@@ -14,16 +16,33 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
+
     redirect_to action: "index"
   end
 
+  def edit
+
+  end
+
+  def update
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated !"
+
+    redirect_to article_path(@article)
+  end
+
     private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
     def article_params
       params.require(:article).permit(:title, :body)
